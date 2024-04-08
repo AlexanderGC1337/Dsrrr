606f8a23318b540764c2cb236a02ca9af1bf4ad9
AlexanderGC1337 <alexanderfrahabbo@gmail.com>
2024-04-08 13:25:08 +0200

    Footer & Favicon update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,42 +1,37 @@
 import React from 'react'
+import Image from 'next/image'
 import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
     return (
         <>
-            <div className='flex justify-between items-center px-5 bg-neutral text-white py-10'>
-                <div className='flex gap-10 items-center ps-5'>
-                    <img src="/assets/images/Logo.jpg" alt="" className='w-64 rounded-xl' />
-                    <div className='uppercase'>
-                        <h3 className='mb-2 font-semibold'>Mere</h3>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                    </div>
-                    <div className='uppercase'>
-                        <h3 className='mb-2 font-semibold'>Nyttige links</h3>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                        <p className='font-extralight'>Lorem ipsum</p>
-                    </div>
-                </div>
-                <div className='flex pe-52 gap-24'>
-                    <div>
-                        <h3 className='uppercase mb-2 -mt-2 font-semibold'>Følg os</h3>
-                        <div className='flex gap-3 mb-2'>
-                            <FaFacebook size={28} /> <FaYoutube size={28} />
-                        </div>
-                        <div className='flex gap-3'>
-                            <FaXTwitter size={28} /> <FaInstagram size={28} />
-                        </div>
-                    </div>
-                    <div>
-                        <h3 className='uppercase mb-2 -mt-2 font-semibold'>Sponsors</h3>
-                    </div>
-                </div>
-            </div>
-            <div className='w-full bg-black text-white text-center py-3 text-xs'>
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <aside>
+                    <a href="/">
+                    <Image src={'/assets/images/Logo.png'} width={200} height={200} alt='Logo'/>
+                    </a>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Mere</h6>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Nyttige links</h6>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                    <a className="link link-hover">Lorem ipsum</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Følg os</h6>
+                    <a href='https://www.facebook.com/profile.php?id=61552746026664' target='_blank' className="link link-hover"><FaFacebook size={28} /></a>
+                </nav>
+            </footer>
+            <div className='w-full bg-black text-base-100 text-center py-3 px-1 text-xs'>
                 <p>
                     © 2024 Dansk SuperRally | All Rights Reserved | Powered by OfficeBoOst
                 </p>
