2f0ccc774815326d46d756f155c6c3720fbbb954
AlexanderGC1337 <alexanderfrahabbo@gmail.com>
2024-04-08 11:19:04 +0200

    Speed insights

--- src/app/components/header/page.jsx
+++ src/app/components/header/page.jsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
     <>
-      <div className="navbar bg-white sticky top-0 uppercase z-50">
+      <div className="navbar bg-base-100 sticky top-0 uppercase z-50">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -21,7 +21,7 @@ export default function Header() {
             </ul>
           </div>
           <Link href={"/"} className="">
-            <img src="/assets/images/Logo.jpg" alt="Logo" className="h-28" />
+            <img src="/assets/images/Logo.png" alt="Logo" className="h-28" />
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
