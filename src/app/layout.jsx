b5465e95bedff90c3dd4513e46a57ec2b4ab88c2
AlexanderGC1337 <alexanderfrahabbo@gmail.com>
2024-04-08 12:34:46 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from "next/font/google";
 import "./globals.css";
