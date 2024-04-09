7f49a118f702f72ba1042cbc11aba41c5d1316f9
AlexanderGC1337 <alexanderfrahabbo@gmail.com>
2024-04-09 13:42:41 +0200

    opdatering på nyheder side

--- src/app/nyheder/page.jsx
+++ src/app/nyheder/page.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 const page = () => {
     return (
         <>
-            <section className='min-h-screen'>
+            <section className='min-h-screen bg-base-100'>
                 <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
                     <a rel="noopener noreferrer" href="/nyheder/topnyhed" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
-                        <img src="/assets/images/jp-racing.jpeg" alt="Rally bil bagfra" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                        <img src="/assets/images/jp-racing.jpeg" alt="Rally bil bagfra" className="object-cover w-full h-64 rounded-l-lg sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                         <div className="p-6 space-y-2 lg:col-span-5">
                             <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Forberedelserne til tv-dækningen af Dansk Super Rally går ind i deres afsluttende fase</h3>
                             <span className="text-xs dark:text-gray-600">1. Februar, 2024</span>
@@ -17,7 +17,7 @@ const page = () => {
                     </a>
                     <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                         <a rel="noopener noreferrer" href="/nyheder/nordicrally" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="/assets/images/nordic-rally.jpeg" alt='Billede fra nordic rally'/>
+                            <img role="presentation" className="object-cover w-full rounded-t-lg h-44 dark:bg-gray-500" src="/assets/images/nordic-rally.jpeg" alt='Billede fra nordic rally'/>
                             <div className="p-6 space-y-2">
                                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">DSR støtter Nordic Rally Championship</h3>
                                 <span className="text-xs dark:text-gray-600">27. December, 2023</span>
